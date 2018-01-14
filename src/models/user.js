ac274c94bda1332d3a98e24c13706a6618f12967
airjp73 <aaron.pettengill.violin@gmail.com>
2018-01-13 19:50:49 -0500

    Changed arrow functions regular functions to fix 'this'

--- src/models/user.js
+++ src/models/user.js
@@ -14,7 +14,7 @@ var userSchema = new mongoose.Schema({
   todos: []
 })
 
-userSchema.methods.setPassword = async (password) => {
+userSchema.methods.setPassword = async function(password) {
   try {
     var salt = await bcrypt.genSalt(8)
     this.password = await bcrypt.hash(password, salt, null)
@@ -24,7 +24,7 @@ userSchema.methods.setPassword = async (password) => {
   }
 }
 
-userSchema.methods.validPassword = async (password) => {
+userSchema.methods.validPassword = async function(password) {
   try {
     if (!this.password)
       throw new Error("Method validPassword -- User data does not contain password")
@@ -42,39 +42,12 @@ userSchema.methods.removeSensitiveInfo = function() {
   this.resetPasswordExpires = undefined
 }
 
-userSchema.methods.genConfirmEmailToken = () => {
+userSchema.methods.genConfirmEmailToken = function() {
   this.confirmEmailToken = crypto.randomBytes(16).toString('hex')
 }
 
-userSchema.methods.genResetPasswordToken = () => {
+userSchema.methods.genResetPasswordToken = function() {
   this.resetPasswordToken = crypto.randomBytes(32).toString('hex')
-}
-
-userSchema.statics.findUserFromRequest = function(field = null, projection = "") {
-
-  return async (req, res, next) => {
-    try {
-
-      var select = {}
-      if (field) {
-        if (!req.body[field])
-          return res.sendStatus(404)
-        select[field] = req.body[field] || ""
-      }
-      else if (req.user)
-        select = {_id: req.user._id}
-      else
-        throw new Error("Cannot find user -- No selection or invalid selection")
-
-      req.user = await this.findOne(select, projection)
-      if (!req.user)
-        return res.sendStatus(404)
-
-      return next()
-    }
-    catch (err) { next(err) }
-
-  }
 }
 
 //create model
