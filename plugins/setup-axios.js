59621742a19e1e71c82f3b4057b6e94cef2b676e
AaronP <aaron.pettengill.violin@gmail.com>
2018-01-19 22:30:54 -0500

    Removed axios plugin
    Not necessary

--- plugins/setup-axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-var options = {}
-
-if (process.server) {
-  options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`
-}
-
-export default axios.create(options)
