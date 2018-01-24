8ce29c99eea82d8569e2c2aba8cd66441382a9f4
airjp73 <aaron.pettengill.violin@gmail.com>
2018-01-23 19:55:36 -0500

    Made update more flexible

--- server/router/todo/controllers/update.js
+++ server/router/todo/controllers/update.js
@@ -2,11 +2,21 @@
 
 module.exports = async (req, res, next) => {
   try {
-    var newTodo = req.body.todo
-    var changes = req.body.changes
+
+    //find todo that should be updated
     var index = req.user.todos.findIndex((todo) => {
-      return todo.text === newTodo.text
+      for (var field in req.body.todo) {
+        if (!todo.hasOwnProperty(field) || todo[field] !== req.body.todo[field])
+          return false
+      }
+      return true
     })
+
+    //if no match found
+    if (index === -1)
+      return res.sendStatus(404).json({message:"Todo not found"})
+
+    //update todo
     Object.assign(req.user.todos[index], changes)
     req.user = await req.user.save()
     res.sendStatus(200)
