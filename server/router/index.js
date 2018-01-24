8dccce43be29959ff5e87e5fd634d67073f3e740
airjp73 <aaron.pettengill.violin@gmail.com>
2018-01-23 20:58:46 -0500

    Added todo router to main router

--- server/router/index.js
+++ server/router/index.js
@@ -3,7 +3,9 @@ var api = express.Router()
 
 //nested routers
 var localLogin = require('./local-login')
+var todo = require('./todo')
 
 api.use(localLogin)
+api.use('/todo', todo)
 
 module.exports = api
