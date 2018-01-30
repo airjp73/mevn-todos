79dbd08bcee8ac260b1fe3ddc2bdcd6d43a02485
airjp73 <aaron.pettengill.violin@gmail.com>
2018-01-29 21:27:34 -0500

    Removed unnecessary require

--- server/router/facebook/index.js
+++ server/router/facebook/index.js
@@ -1,11 +1,6 @@
 var express = require("express")
 var passport = require('passport')
 var api = express.Router()
-var User = require("../../models/user.js")
-
-//custom middleware
-var requireLoggedIn = require("../middleware/requireLoggedIn.js")
-var requireFields = require("require-fields")
 
 //passport strategies
 var configPassport = require('./configPassport')
